a6a05b38c5aa3b18a061d5435d6219b5132910e4
9956 <zhz995617@gmail.com>
2021-04-11 22:51:07 +0800

    feat: 整数反转

--- 1.两数之和.js
+++ 1.两数之和.js
@@ -3,6 +3,8 @@
  * @param {number} target
  * @return {number[]}
  */
+
+//暴力解 O(n^2)
 var twoSum = function(nums, target) {
   for(let i = 0; i < nums.length; i++) {
     for(let j = 0; j < nums.length; j++) {
---
+++ 7.整数反转.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+
+//通过取余反转数字结果，判断反转后是否超过32位的范围 2147483647 -2147483648
+var reverse = function(x) {
+  let result = 0;
+  let isSign;
+
+    isSign = x > 0 ? '' : '-';
+    x = x > 0 ? x : Math.abs(x);
+
+  while(x !== 0) {
+    result = result * 10 + x % 10;
+    if (result > Math.pow(2, 31) - 1 || result < Math.pow(-2, 31)) {
+      return 0;
+    }
+    x = Math.floor(x / 10);
+  }
+
+  return isSign + result;
+};
