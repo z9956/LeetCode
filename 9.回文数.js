bd9122b9a5b10886d75e81715e126116cf851d30
9956 <zhz995617@gmail.com>
2021-04-12 22:44:45 +0800

    feat: 回文数

---
+++ 9.回文数.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+  if (x === 0) return true;
+  if (x < 0 || x % 10 === 0) return false;
+
+  let num = 0;
+  while(x > num) {
+    num = num * 10 + x % 10;
+    x = Math.floor(x / 10);
+  }
+
+  return num === x || x === Math.floor(num / 10);
+};
+
+//简单粗暴
+var isPalindrome2 = function(x) {
+  const reverseResult = +String(x).split('').reverse().join('');
+
+  return x === reverseResult;
+};
+
+console.log(isPalindrome(12321));
