ee1aa6ceba0bc6da8f4a00da36dd4ebbadd795d1
9956 <zhz995617@gmail.com>
2021-04-14 00:40:06 +0800

    feat: 删除有序列表数组中的重复项

---
+++ 26.删除有序数组中的重复项.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// 有序列表，原地修改，双指针比对
+var removeDuplicates = function(nums) {
+  if(!nums.length) return 0;
+  let i = 0;
+  for(let j = 1; j < nums.length; j++){
+    if(nums[j] !== nums[i]){
+      i++;
+      nums[i] = nums[j];
+    }
+  }
+  return i + 1;
+};
