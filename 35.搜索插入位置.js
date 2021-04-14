1cf83d3eb8401167e406ca882beef0e472dd29d3
9956 <zhz995617@gmail.com>
2021-04-15 01:40:49 +0800

    feat: 搜索插入位置

---
+++ 35.搜索插入位置.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+  const n = nums.length;
+  let left = 0, right = n - 1;
+  while (left <= right) {
+    let mid = ~~((left + right) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] > target) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return left;
+};
