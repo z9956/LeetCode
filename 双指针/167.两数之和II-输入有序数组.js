c46756fde85131f3716c07b2eaf8db60bf2bdb0d
9956 <zhz995617@gmail.com>
2021-05-04 23:27:06 +0800

    feat: 两数之和II-输入有序数组

---
+++ 双指针/167.两数之和II-输入有序数组.js
@@ -0,0 +1,47 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+//有序数组,首尾双时针查找 O(N)
+var twoSum = function (numbers, target) {
+  let i = 0;
+  let j = numbers.length - 1;
+
+  while (i < j) {
+    const sum = numbers[i] + numbers[j];
+    if (sum === target) return [i + 1, j + 1];
+
+    sum > target ? j-- : i++;
+  }
+};
+
+var twoSum2 = function (numbers, target) {
+  let i = 0;
+  let j = 1;
+
+  while (i < numbers.length) {
+    if (numbers[i] + numbers[j] === target) return [i + 1, j + 1];
+
+    if (j === numbers.length - 1) {
+      i++;
+      j = i + 1;
+    } else {
+      j++;
+    }
+  }
+};
+
+/*
+ * 输入：numbers = [2,7,11,15], target = 9
+ * 输出：[1,2]
+ *
+ * 输入：numbers = [2,3,4], target = 6
+ * 输出：[1,3]
+ *
+ * 输入：numbers = [-1,0], target = -1
+ * 输出：[1,2]
+ *
+ * [1, 2, 3, 4, 4, 9, 56, 90]
+ * 8
+ * */
