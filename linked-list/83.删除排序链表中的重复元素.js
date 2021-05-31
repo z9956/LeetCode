4dc6efe666a3a73e28a19a1463f2301eb2a154b2
9956 <zhz995617@gmail.com>
2021-05-31 22:35:14 +0800

    feat: add linked list

---
+++ linked-list/83.删除排序链表中的重复元素.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function (head) {
+  let currentNode = head;
+
+  while (currentNode) {
+    if (currentNode.val === currentNode.next.val) {
+      currentNode.next = currentNode.next.next;
+    }
+
+    currentNode = currentNode.next;
+  }
+};
+
+/*
+ * 输入：head = [1,1,2]
+ * 输出：[1,2]
+ * */
