851256a688045bd106a06645a2aede8caf3c5575
9956 <zhz995617@gmail.com>
2021-06-08 00:35:07 +0800

    feat: 237.删除链表中的节点

---
+++ linked-list/237.删除链表中的节点.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function (node) {
+  node.val = node.next.val;
+  node.next = node.next.next;
+};
+
+/*
+ * 输入：head = [4,5,1,9], node = 5
+ * 输出：[4,1,9]
+ * */
