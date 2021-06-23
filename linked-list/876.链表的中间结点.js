36dd2ab9ca8b75e7146ece67ad542f9465a97ce9
9956 <zhz995617@gmail.com>
2021-06-23 23:43:02 +0800

    feat: 876.链表的中间结点

---
+++ linked-list/876.链表的中间结点.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function (head) {
+  let results = [];
+
+  let current = head;
+
+  while (current) {
+    results.push(current);
+
+    current = current.next;
+  }
+
+  return results[results.length >> 1];
+};
+
+var middleNodeTwo = function (head) {
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return slow;
+};
