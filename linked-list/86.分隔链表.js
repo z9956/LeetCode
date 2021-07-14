99c3940d2362c9ceb0141a614fb5d9f66d8b7189
9956 <zhz995617@gmail.com>
2021-07-15 00:36:00 +0800

    feat: 86.分隔链表

---
+++ linked-list/86.分隔链表.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} x
+ * @return {ListNode}
+ */
+var partition = function(head, x) {
+  let small = new ListNode(0);
+  let large = new ListNode(0);
+  let smallList = small;
+  let largeList = large;
+
+  let current = head;
+
+  while (current) {
+    if (current.val < x) {
+      small.next = current;
+      small = small.next;
+    } else {
+      large.next = current;
+      large = large.next;
+    }
+
+    current = current.next;
+  }
+
+  large.next = null;
+  small.next = largeList.next;
+  return smallList.next;
+};
