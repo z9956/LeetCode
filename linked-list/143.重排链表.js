3647ca336967eaa9f129b7e03875fe39b0a10cd9
9956 <zhz995617@gmail.com>
2021-08-03 23:47:02 +0800

    feat: 143.重排链表

---
+++ linked-list/143.重排链表.js
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {void} Do not return anything, modify head in-place instead.
+ */
+var reorderList = function (head) {
+  const dummy = new ListNode(0);
+  dummy.next = head;
+
+  let slow = dummy;
+  let fast = dummy;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  let right = slow.next;
+  let left = dummy.next;
+  slow.next = null;
+
+  right = reverseList(right);
+
+  while (left && right) {
+    let leftNext = left.next;
+    let rightNext = right.next;
+
+    right.next = left.next;
+    left.next = right;
+    left = leftNext;
+    right = rightNext;
+  }
+
+  return dummy.next;
+};
+
+var reverseList = (head) => {
+  let prev = null;
+  let cur = head;
+
+  while (head) {
+    let next = cur.next;
+    cur.next = prev;
+
+    prev = cur;
+    cur = next;
+  }
+
+  return prev;
+};
