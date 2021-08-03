c4502951c20bf24f0b0f0306326d6e4a5f1ee315
9956 <zhz995617@gmail.com>
2021-08-03 23:42:39 +0800

    feat: add reverseList

--- linked-list/206.反转链表.js
+++ linked-list/206.反转链表.js
@@ -23,6 +23,21 @@ var reverseList = function (head) {
   return prevNode;
 };
 
+var reverseListTwo = (head) => {
+  let prev = null;
+  let cur = head;
+
+  while (cur) {
+    let next = cur.next;
+    cur.next = prev;
+
+    prev = cur;
+    cur = next;
+  }
+
+  return prev;
+};
+
 /*
  * 输入：head = [1,2,3,4,5]
  * 输出：[5,4,3,2,1]
