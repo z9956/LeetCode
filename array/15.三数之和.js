fc34d43959eaea0ef8abd4c89fed522b5c8ccb16
z9956 <zhz995617@gmail.com>
2021-10-21 23:53:25 +0800

    feat: 15.三数之和

---
+++ array/15.三数之和.js
@@ -0,0 +1,52 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function (nums) {
+	nums = nums.sort().sort((a, b) => a - b);
+
+	if (nums.length < 3 || !nums.length) return [];
+
+	let results = [];
+
+	for (let i = 0; i < nums.length; i++) {
+		if (nums[i] > 0) break;
+
+		let first = i + 1;
+		let last = nums.length - 1;
+
+		if (i > 0 && nums[i] === nums[i - 1]) continue;
+		while (first < last) {
+			const sum = nums[i] + nums[first] + nums[last];
+
+			if (sum === 0) {
+				results.push([nums[i], nums[first], nums[last]]);
+
+				while (first < last && nums[first] === nums[first + 1]) first++;
+				while (last < last && nums[last] === nums[last - 1]) last--;
+				first++;
+				last--;
+			} else if (sum < 0) {
+				first++;
+			} else if (sum > 0) {
+				last--;
+			}
+		}
+	}
+
+	return results;
+};
+
+// console.log(threeSum([-1, 0, 1, 2, -1, -4]));
+// console.log(threeSum([-2, 0, 0, 2, 2]));
+/*
+ * [-4, -1, -1, 0, 1, 2]
+ *
+ * cur left right
+ * -4 2 -1
+ * -1 -4 -1
+ * -1 -1 0
+ * 0 -1 1
+ * 1 0 2
+ * 2 1 -4
+ * */
