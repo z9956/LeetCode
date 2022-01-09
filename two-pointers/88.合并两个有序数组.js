3632b83b09d5bb7911c8bf69821b361e083a285e
z9956 <zhz995617@gmail.com>
2022-01-09 23:27:12 +0800

    style: style

--- two-pointers/88.合并两个有序数组.js
+++ two-pointers/88.合并两个有序数组.js
@@ -10,26 +10,24 @@ var merge = function (nums1, m, nums2, n) {
 	let i = m - 1;
 	let j = n - 1;
 	let k = m + n - 1;
-	//
+
 	while (i >= 0 || j >= 0) {
-		while (i >= 0 || j >= 0) {
-			if (i < 0) {
-				nums1[k] = nums2[j];
-				k--;
-				j--;
-			} else if (j < 0) {
-				nums1[k] = nums1[i];
-				k--;
-				i--;
-			} else if (nums1[i] < nums2[j]) {
-				nums1[k] = nums2[j];
-				k--;
-				j--;
-			} else {
-				nums1[k] = nums1[i];
-				k--;
-				i--;
-			}
+		if (i < 0) {
+			nums1[k] = nums2[j];
+			k--;
+			j--;
+		} else if (j < 0) {
+			nums1[k] = nums1[i];
+			k--;
+			i--;
+		} else if (nums1[i] < nums2[j]) {
+			nums1[k] = nums2[j];
+			k--;
+			j--;
+		} else {
+			nums1[k] = nums1[i];
+			k--;
+			i--;
 		}
 	}
 };
